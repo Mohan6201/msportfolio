7f291fd3e31c89bea4ae9793da7e99d711edf69d
Mohan6201 <mgnsrvn100@gmail.com>
2025-05-13 15:46:37 +0530

    Updated Certificates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
+import CertificateCard from "./components/certificatesSection/CertificateCard";
+import CertificateMain from "./components/certificatesSection/CertificateMain";
+import CertificatesMain from "./components/certificatesSection/CertificateMain";
 import ContactMeMain from "./components/contactMeSection/ContactMeMain";
 import ExperienceMain from "./components/experienceSection/ExperienceMain";
 import FooterMain from "./components/footer/FooterMain";
@@ -6,6 +9,7 @@ import HeroGradient from "./components/heroSection/HeroGradient";
 import HeroMain from "./components/heroSection/HeroMain";
 import NavbarMain from "./components/navbar/NavbarMain";
 import ProjectsMain from "./components/projectsSection/ProjectsMain";
+import ResumeMain from "./components/resumeSection/ResumeMain";
 import SkillsMain from "./components/skillsSection/SkillsMain";
 import SubSkills from "./components/skillsSection/SubSkills";
 import SubHeroMain from "./components/subHeroSection/SubHeroMain";
@@ -20,8 +24,10 @@ function App() {
       <AboutMeMain />
       <SkillsMain />
       <SubSkills />
+      <CertificateMain />
       <ExperienceMain />
       <ProjectsMain />
+      <ResumeMain />
       <ContactMeMain />
       <FooterMain />
     </main>
