dca40d3b1e329badafe42dac9ef1b674c8cb2c68
Mohan6201 <mgnsrvn100@gmail.com>
2025-06-19 11:58:37 +0530

    Committed Github

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -7,7 +7,7 @@ const ContactSocial = () => {
   return (
     <div className="flex gap-4">
       <SingleContactSocial link="https://www.linkedin.com/in/mohan6201"   target="_blank"  rel="noopener noreferrer"   Icon={FaLinkedinIn} />
-      <SingleContactSocial link="https://github.com/Mohan6201"  Icon={FiGithub} />
+      <SingleContactSocial link="https://github.com/Mohan6201" target="_blank"  rel="noopener noreferrer" Icon={FiGithub} />
       <SingleContactSocial link="#"  Icon={FaInstagram} />
     </div>
   );
